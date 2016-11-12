6e83f9742e350f9c0a92e355c841614e9a06bd0e
Andre Ramoni <ramoni@Andres-iMac.local>
2016-11-12 15:15:20 -0200

    Converting user roulette to resource instead of class.

--- lib/facter/roulette.rb
+++ lib/facter/roulette.rb
@@ -1,10 +1,10 @@
-Facter.add("user_roulette") do
+Facter.add("roulette_user") do
   confine :kernel => 'Linux'
   setcode do
     Facter::Util::Resolution.exec('cat /etc/passwd | shuf -n 1 | cut -d: -f1')
   end
 end
-Facter.add("file_roulette") do
+Facter.add("roulette_file") do
   confine :kernel => 'Linux'
   setcode do
     Facter::Util::Resolution.exec('locate / | shuf -n 1')
