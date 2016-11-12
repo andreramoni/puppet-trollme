df433991d7d2746dd589db907f04c96365962fb9
Andre Ramoni <ramoni@Andres-iMac.local>
2016-11-12 14:33:43 -0200

    Fixed a bug in the user_roulette fact.

--- lib/facter/user_roulette.rb
+++ lib/facter/user_roulette.rb
@@ -1,6 +1,6 @@
 Facter.add("user_roulette") do
   confine :kernel => 'Linux'
   setcode do
-    Facter::Util::Resolution.exec('cat /etc/passwd | shuft -n 1 | cut -d: -f1')
+    Facter::Util::Resolution.exec('cat /etc/passwd | shuf -n 1 | cut -d: -f1')
   end
 end
