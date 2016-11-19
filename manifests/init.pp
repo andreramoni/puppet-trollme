dd6cda8c3f823e5f2d829c4adf08e32dca60f501
Andre Ramoni <andre.ramoni@gmail.com>
2016-11-19 13:39:03 -0200

    Installs and creates locate. Needed by roulette facts.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,10 +2,23 @@
 #
 # This class does nothing.
 #
-class trollme {
+class trollme (
+  $locate_pkg = $::trollme::params::locate_pkg,
+  $locate_db = $::trollme::params::locate_db,
+) inherits trollme::params {
   if  ($::osfamily != 'Debian')
   and ($::osfamily != 'RedHat') {
     fail("Module ${module_name} does not support ${::osfamily}.")
+  }
+
+  # locate must be installed for roulette facts to work.
+  package { $locate_pkg:
+    ensure => 'installed',
+  }
+  exec { 'locatedb':
+    command => '/bin/updatedb',
+    creates => $locate_db,
+    require => Package[$locate_pkg]
   }
 
 }
