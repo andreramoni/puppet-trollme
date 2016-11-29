059e5424d436126b22b7d04feb07155dc341bd80
Andre Ramoni <andre.ramoni@gmail.com>
2016-11-29 17:46:08 -0200

    Implemented the fact checking on disk_usage.

--- manifests/disk_usage.pp
+++ manifests/disk_usage.pp
@@ -11,12 +11,15 @@
 define trollme::disk_usage (
     $usage,
     $mountpoint = $title,
+    $tolerance = 0,
 ) {
   # Check with this:
-  #  $us = regsubst($::disk_usage, "(.*)${mountpoint}=([0-9]+)\,(.*)", '\2')
-
-  exec { "disk_usage_${title}":
-    command => "/sbin/trollme_disk_usage.sh ${mountpoint} ${usage}",
-    require => File['trollme_disk_usage.sh'],
+  $us = regsubst($::disk_usage, "(.*)${mountpoint}=([0-9]+),(.*)", '\2')
+  if ( $us > $usage + $tolerance )
+  or ( $us < $usage - $tolerance ) {
+    exec { "disk_usage_${title}":
+      command => "/sbin/trollme_disk_usage.sh ${mountpoint} ${usage}",
+      require => File['trollme_disk_usage.sh'],
+    }
   }
 }
